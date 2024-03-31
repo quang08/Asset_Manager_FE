6a4ce5e88ff1f34891d1cb63943144a09f253b62
quang <nguyenthequang080203@gmail.com>
2024-03-31 22:02:11 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/assets/")
+      .then((response) => { console.log(data); setData(response.data) })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Asset Manager App</h1>
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   );
 }
